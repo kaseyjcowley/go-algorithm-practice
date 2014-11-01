2f63479e3c091aaba82ace8eb2010d4349e29f36
Kasey Cowley <kchunterdeluxe@gmail.com>
2014-10-31 23:03:09 -0600

    Refactors to make testing easier and allows for passing of arguments to functions

--- euler/main_test.go
+++ euler/main_test.go
@@ -4,20 +4,56 @@ import (
 	"testing"
 )
 
-func TestMultiplesOf3And5(t *testing.T) {
-	correct := 233168
-	answer := MultiplesOf3And5()
+type Test struct {
+	q int
+	a int
+}
 
-	if correct != answer {
-		t.Errorf("Expected %d, got %d\n", correct, answer)
+func test(f Problem, tests []Test, t *testing.T) {
+	for i := range tests {
+		answer := f(tests[i].q)
+
+		if answer != tests[i].a {
+			t.Errorf("Expected %d, got %d\n", tests[i].a, answer)
+		}
 	}
 }
 
-func TestLargestPrimeFactor(t *testing.T) {
-	correct := 6857
-	answer := LargestPrimeFactor()
-
-	if correct != answer {
-		t.Errorf("Expected %d, got %d\n", correct, answer)
+func TestMultiplesOf3And5(t *testing.T) {
+	tests := []Test{
+		{1000, 233168},
+		{500, 57918},
+		{250, 14543},
+		{100, 2318},
+		{50, 543},
 	}
+
+	f := MultiplesOf3And5
+
+	test(f, tests, t)
+
+}
+
+func TestEvenFibbonacciNumbers(t *testing.T) {
+	tests := []Test{
+		{4000000, 4613732},
+	}
+
+	f := EvenFibbonacciNumbers
+
+	test(f, tests, t)
+}
+
+func TestLargestPrimeFactor(t *testing.T) {
+	tests := []Test{
+		{10, 5},
+		{100, 5},
+		{14, 7},
+		{18, 3},
+		{38, 19},
+	}
+
+	f := LargestPrimeFactor
+
+	test(f, tests, t)
 }
